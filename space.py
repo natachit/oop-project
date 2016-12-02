0d065055f4a00691f2e11b082e63fc92e88918bd
natachit <natachit@hotmail.com>
2016-12-02 14:44:40 +0700

    add class ModelSprite

--- space.py
+++ space.py
@@ -5,12 +5,25 @@ from models import World, Ship
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
 
+class ModelSprite(arcade.Sprite):
+    def __init__(self, *args, **kwargs):
+        self.model = kwargs.pop('model', None)
+        super().__init__(*args, **kwargs)
+ 
+    def sync_with_model(self):
+        if self.model:
+            self.set_position(self.model.x, self.model.y)
+ 
+    def draw(self):
+        self.sync_with_model()
+        super().draw()
+
 class SpaceGameWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
         arcade.set_background_color(arcade.color.BLACK)
         self.world = World(width, height)
-        self.ship_sprite = arcade.Sprite('images/ship.png')
+        self.ship_sprite = ModelSprite('images/ship.png',model=self.world.ship)
 
     def on_draw(self):
         arcade.start_render()
@@ -18,7 +31,6 @@ class SpaceGameWindow(arcade.Window):
  
     def animate(self, delta):
         self.world.animate(delta)
-        self.ship_sprite.set_position(self.world.ship.x, self.world.ship.y)
 
 if __name__ == '__main__':
     window = SpaceGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
