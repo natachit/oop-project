bb13808e7e7a2ffc5628389209f39cb1645f45e6
natachit <natachit@hotmail.com>
2016-12-02 14:08:41 +0700

    ship can move

--- space.py
+++ space.py
@@ -6,13 +6,6 @@ SCREEN_HEIGHT = 600
 class SpaceGameWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
-        arcade.set_background_color(arcade.color.BLACK)
-
-    def on_draw(self):
-        arcade.start_render()
-
-    def __init__(self, width, height):
-        super().__init__(width, height)
  
         arcade.set_background_color(arcade.color.BLACK)
  
@@ -21,8 +14,13 @@ class SpaceGameWindow(arcade.Window):
 
     def on_draw(self):
         arcade.start_render()
- 
         self.ship.draw()
+
+    def animate(self, delta):
+        ship = self.ship
+        if ship.center_y > SCREEN_HEIGHT:
+            ship.center_y = 0
+        self.ship.set_position(self.ship.center_x, self.ship.center_y + 5)
 
 if __name__ == '__main__':
     window = SpaceGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
